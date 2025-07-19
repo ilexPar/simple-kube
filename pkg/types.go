310b2bf1dabd8980ade3c69757de96652a82df34
alex <awal.myers@gmail.com>
2025-07-19 15:54:46 -0300

    fix: update ClientInterface

--- pkg/types.go
+++ pkg/types.go
@@ -1,11 +1,16 @@
 package simplekube
 
 import (
+	"context"
+
 	"github.com/ilexPar/simple-kube/pkg/cluster"
 	"github.com/ilexPar/simple-kube/pkg/namespaced"
+	"k8s.io/client-go/kubernetes"
 )
 
 type ClientInterface interface {
-	NamespacedQuery(namespace string) namespaced.QueryNamespace
-	ClusterQuery() cluster.QueryCluster
+	Config(ctx context.Context, client kubernetes.Interface) ClientInterface
+
+	cluster.QueryCluster
+	InNamespace(namespace string) namespaced.QueryNamespace
 }
